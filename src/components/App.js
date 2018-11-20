c42ab1717da3eb731a00b53ce43c38c59bc31b3e
DonovanDwyer <dwyer.donovan@gmail.com>
2018-11-20 16:08:00 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,43 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (petType) => {
+    this.setState({
+      filters: {
+        type: petType
+      }
+    })
+  }
+
+
+  onFindPetsClick = (query = "pets") => {
+    this.state.filters.type === 'dog' ? query = "pets?type=" + this.state.filters.type : null;
+    this.state.filters.type === 'cat' ? query = "pets?type=" + this.state.filters.type : null;
+    this.state.filters.type === 'micropig' ? query = "pets?type=" + this.state.filters.type : null;
+    fetch(`/api/${query}`)
+      .then(res => res.json())
+      .then(json => {
+        query === "pets" ? (
+          this.setState({pets: json})
+        ) : (
+        this.setState({
+          pets: json.filter(pet => pet.type === this.state.filters.type)
+        }))
+      });
+  }
+
+
+
+  onAdoptPet = (petId) => {
+    let findPet = [...this.state.pets].find(pet => pet.id === petId);
+    let newPets = [...this.state.pets].filter(pet => pet.id !== petId)
+    findPet.isAdopted = true;
+    this.setState({
+      pets: [findPet, ...newPets]
+    })
+    console.log([findPet, ...newPets]);
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +61,13 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+              onChangeType={this.onChangeType}
+              onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
