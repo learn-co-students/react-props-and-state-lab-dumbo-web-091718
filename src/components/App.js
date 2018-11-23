fda1806edbee1e6ad2ae03e3a5ec13c9c625e0a9
dianajyoo <diana.yooj@gmail.com>
2018-11-23 18:04:57 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,89 @@ class App extends React.Component {
     }
   }
 
+  findPetsClickHandler = () => {
+
+    if (this.state.filters.type === 'all') {
+      fetch('/api/pets')
+        .then(res => res.json())
+        .then(json => {
+          this.state.pets = []
+
+          json.forEach(petObj => {
+            this.setState({
+              pets: [...this.state.pets, petObj]
+            })
+          })
+          // console.log(this.state.pets)
+        })
+    } else if (this.state.filters.type === 'cat') {
+      fetch('/api/pets?type=cat')
+        .then(res => res.json())
+        .then(json => {
+          this.state.pets = []
+
+          json.forEach(catObj => {
+            this.setState({
+              pets: [...this.state.pets, catObj]
+            })
+          })
+          // console.log(this.state.pets)
+        })
+    } else if (this.state.filters.type === 'dog') {
+      fetch('/api/pets?type=dog')
+        .then(res => res.json())
+        .then(json => {
+          this.state.pets = []
+
+          json.forEach(dogObj => {
+            this.setState({
+              pets: [...this.state.pets, dogObj]
+            })
+          })
+          // console.log(this.state.pets)
+        })
+    } else if (this.state.filters.type === 'micropig') {
+      fetch('/api/pets?type=micropig')
+        .then(res => res.json())
+        .then(json => {
+          this.state.pets = []
+
+          json.forEach(pigObj => {
+            this.setState({
+              pets: [...this.state.pets, pigObj]
+            })
+          })
+          // console.log(this.state.pets)
+        })
+    }
+
+  }
+
+  changeTypeHandler = event => {
+
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: event.target.value
+      }
+    })
+
+    // console.log(event.target.value) --> cat, dog, etc.
+    // console.log(this.state.pets)
+    // console.log(this.state.filters)
+
+  }
+
+  adoptPetHandler = (id) => {
+
+    const pets = this.state.pets.map(pet => {
+      return pet.id === id ? { ...pet, isAdopted: true } : pet
+    })
+
+    this.setState({ pets })
+
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +107,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.changeTypeHandler} onFindPetsClick={this.findPetsClickHandler} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.adoptPetHandler} />
             </div>
           </div>
         </div>
